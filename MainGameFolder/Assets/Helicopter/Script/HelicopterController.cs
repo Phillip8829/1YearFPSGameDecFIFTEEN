0e72a517b9638dfb339b90c41c414c3acf699318
Phillip <phillipyates12@hotmail.com>
2022-12-18 16:38:59 -0600

    Helicopter Feels Much More Stable

--- MainGameFolder/Assets/Helicopter/Script/HelicopterController.cs
+++ MainGameFolder/Assets/Helicopter/Script/HelicopterController.cs
@@ -14,7 +14,7 @@ public class HelicopterController : MonoBehaviour
     public float liftForce = 10.0f;
 
     // Set the amount of force applied to the helicopter when it moves left or right
-    public float lateralForce = 0.01f;
+    public float lateralForce = 0.005f;
 
     // Set the helicopter's rotor blade GameObject
     public GameObject rotorBlade;
@@ -46,15 +46,25 @@ public class HelicopterController : MonoBehaviour
         else if (Input.GetKey(KeyCode.S))
         {
             // Apply a downward force to the helicopter
-             rb.AddForce(Vector3.down * liftForce);
+             rb.AddForce(Vector3.down * liftForce * 10f);
              rb.constraints = RigidbodyConstraints.None;
+
+        }
+        else if (Input.GetKey(KeyCode.C))
+        {
+            rb.AddForce(transform.rotation * (new Vector3(0,0,1)) * 50f);
+            rb.constraints = RigidbodyConstraints.None;
+            rb.constraints = RigidbodyConstraints.FreezePositionX;
+            rb.constraints = RigidbodyConstraints.FreezeRotationX;
 
         }
         else if (Input.GetKey(KeyCode.Space))
         {
             // Apply a forward force to the helicopter
-            rb.AddForce(-transform.forward * movementSpeed * 10);
+            rb.AddForce(transform.rotation * (new Vector3(0,0,-1)) * 50f);
             rb.constraints = RigidbodyConstraints.None;
+            rb.constraints = RigidbodyConstraints.FreezePositionX;
+            rb.constraints = RigidbodyConstraints.FreezeRotationX;
 
         }
 
@@ -62,13 +72,13 @@ public class HelicopterController : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             // Apply a force to the left of the helicopter
-            rb.AddForce(Vector3.left * lateralForce);
+            rb.AddForce(transform.rotation * -Vector3.right);
             rb.constraints = RigidbodyConstraints.None;
         }
         else if (Input.GetKey(KeyCode.A))
         {
             // Apply a force to the right of the helicopter
-            rb.AddForce(Vector3.right * lateralForce);
+            rb.AddForce(transform.rotation *  -Vector3.left);
             rb.constraints = RigidbodyConstraints.None;
         }
 
@@ -76,12 +86,12 @@ public class HelicopterController : MonoBehaviour
         if (Input.GetKey(KeyCode.Q))
         {
             // Rotate the helicopter left
-            transform.Rotate(Vector3.down * movementSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.down * movementSpeed * Time.deltaTime * 5);
         }
         else if (Input.GetKey(KeyCode.E))
         {
             // Rotate the helicopter right
-            transform.Rotate(Vector3.up * movementSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.up * movementSpeed * Time.deltaTime * 5);
         }
 
         // Check if the helicopter is tilted or rolled too far
@@ -98,7 +108,7 @@ public class HelicopterController : MonoBehaviour
     void FixedUpdate()
     {
         // Spin the helicopter's rotor blade
-        rotorBlade.transform.Rotate(Vector3.forward * movementSpeed * Time.deltaTime * 300);
+        rotorBlade.transform.Rotate(Vector3.forward * movementSpeed * Time.deltaTime * 200);
         
         // Apply a constant upward force to the helicopter to make it hover
         rb.AddForce(Vector3.up * hoverForce);
